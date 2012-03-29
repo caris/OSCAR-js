983e12254a454d03d7d71f1bbf7a4952311b0b28
Troy Coburn <troy.coburn@caris.com>
2012-03-29 08:46:52 -0300

    SFV-1083
    pom.xml
    Added EasyMock library for testing.
    
    TestDownloadService.class
    
    Unit test for the proxy.

---
+++ oscarexchange4j/src/test/java/com/caris/oscarexchange4j/proxy/TestDownloadService.java
@@ -0,0 +1,40 @@
+/**
+ * CARIS oscar - Open Spatial Component ARchitecture
+ *
+ * Copyright 2012 CARIS <http://www.caris.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.caris.oscarexchange4j.proxy;
+
+import static org.junit.Assert.*;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+public class TestDownloadService {
+    @Test
+    public void testDownloadService() throws Exception {
+        String testString = new String("some random works that are not so random.");
+        byte[] byteResponse = testString.getBytes();
+        
+        DownloadService ds = EasyMock.createMock(DownloadService.class);
+        EasyMock.expect(ds.run()).andReturn(byteResponse);
+        EasyMock.replay(ds);
+        
+        byte[] b = ds.run();
+        
+        String stringResponse = new String(b);
+        assertTrue(stringResponse.equals(testString));
+    }
+}
