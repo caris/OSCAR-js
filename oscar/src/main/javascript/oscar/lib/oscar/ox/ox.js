dd1f7a62e127da8b6500a1561cfeb0e255f33c54
jeremybourgeois <jeremy.bourgeois@caris.com>
2012-11-23 11:48:34 -0400

    SFV-1207 Added an if statement.

--- oscar/src/main/javascript/oscar/lib/oscar/ox/ox.js
+++ oscar/src/main/javascript/oscar/lib/oscar/ox/ox.js
@@ -791,7 +791,8 @@ oscar.ox.Layer = oscar.BaseClass( {
 			params.layers = layerNamesArr;
 			params.format = (this.format) ? this.format : "image/png";
 			params.transparent = true;
-			params.version = this.parameters.version;
+			if (this.parameters.version)
+				params.version = this.parameters.version;
 			olLayer = new OpenLayers.Layer.WMS(this.name, this.urls, params, {
 				wrapDateLine :false,
 				buffer :0,
