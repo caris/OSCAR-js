787afb27e6006aef715506b5aad902ced9beecb8
troycoburn <troy.coburn@caris.com>
2013-06-27 12:22:24 -0300

    SFD-7
    Updating CatalogueResults.js UI.

--- oscar/src/main/javascript/oscar/lib/oscar/Gui/CatalogueResults.js
+++ oscar/src/main/javascript/oscar/lib/oscar/Gui/CatalogueResults.js
@@ -37,130 +37,235 @@ oscar.Gui.CatalogueResults = new oscar.BaseClass(oscar.Gui,{
     },
     draw:function() {
         oscar.Gui.prototype.draw.apply(this);
-    },
-    showResults:function(results) {
-        $$(this.div).empty();
-        $$(this.div).scrollTop(0);
-        var layer = map.getLayersByName("results")[0].removeAllFeatures();
-        this.features = [];
-        var records = results.records;
-        for(var r in  records) {
-            var record = records[r];
-            var feature = this.getFeatureFromRecord(record);
-            
-            record.feature = feature.clone();
-            
-            if(feature.geometry.getArea() > 0) {
-                this.features.push(feature);
-            }
-            this.addRecordToResultList(record);
-        }
-        this.renderFeaturesToMap();
-    },
-    showSearchInfo:function(info) {
-        var $div = $$(this.div);
-        var $infoDiv = $$("<div></div");
-        $div.append($infoDiv);
-    },
-    enablePagination:function(info) {
-        var nextRecord = info.nextRecord;
-        var $previous = $$("<button>Previous</button>");
-        var $next = $$("<button>Next</button>");
-        var $results = $$(this.div);
-        $results.append($previous);
-        $results.append($next);
+        this.buildLayout();
         var scope = this;
-        
-        var nextDisabled=(info.nextRecord > info.numberOfRecordsMatched)?true:false;
-        $next.button({
+        setTimeout(function() {
+            var x = $$(scope.div).layout({
+                closable:false,
+                resizable:false,
+                center__showOverflow:true,
+                onresize:function() {
+                    scope.results.slimScroll({
+                        height:"auto",
+                        scrollTo:0
+                    });
+                }
+            });
+        },0);
+    },
+    buildLayout:function() {
+        this.layout = {
+            north:null,
+            center:null,
+        }
+        this.layout.north = $$("<div></div");
+        this.layout.north.addClass("ui-layout-north");
+        this.layout.center = $$("<div></div");
+        this.layout.center.addClass("ui-layout-center");
+        $$(this.div).append(this.layout.north);
+        $$(this.div).append(this.layout.center);
+        this.results = $$("<div></div>");
+        this.layout.center.append(this.results);
+        this.$searchInfo = $$("<div>&nbsp;</div>");
+        this.$previous = $$("<button>Previous</button>");
+        this.$next = $$("<button>Next</button>");
+        var $results = $$(this.div);
+        var scope = this;
+        this.$buttons = $$("<div></div>");
+        this.$buttons.css({
+            "position":"relative"
+        });
+        this.$buttons.append(this.$searchInfo);
+        this.$next.button({
             icons : {
                 primary : "ui-icon-triangle-1-e"
             },
             text : false,
-            disabled:nextDisabled
+            disabled:true
         }).click(function() {
             scope.events.triggerEvent("next");
         });
-        $next.css({"float":"right"});
+        this.$next.css({
+            "position":"absolute",
+            "right":"0px"
+        });
         
-        var previousDisabled=(info.nextRecord - info.numberOfRecordsReturned -1 <=0)?true:false;
-        $previous.button({
+        this.$previous.button({
             icons : {
                 primary : "ui-icon-triangle-1-w"
             },
             text : false,
-            disabled:previousDisabled
+            disabled:true
         }).click(function() {
             scope.events.triggerEvent("previous");
+        }); 
+
+        this.$previous.css({
+            "position":"relative",
+            "left":"0px"
         });
+        
+        this.$searchInfo.css({
+            "position":"absolute",
+            "left":"35px",
+            "top":"2px",
+            "width":"162px",
+            "height":"25px",
+            "text-align":"center"
+        });
+        this.$searchInfo.addClass("searchResultInformation");
+        this.$buttons.append(this.$next);
+        this.$buttons.append(this.$previous);
+        this.layout.north.append(this.$buttons);
+        this.$buttons.css("visibility","hidden");
+        
+        
+        
+    },
+    showResults:function(results) {
+        this.layout.center.scrollTop();
+        this.results.empty();
+        var layer = map.getLayersByName("results")[0].removeAllFeatures();
+        this.features = [];
+        var records = results.records;
+        this.showSearchInfo(results.SearchResults);
+        if(records.length == 0) {
+            this.$searchInfo.html(oscar.i18n("map.information.no.records.found"));
+            return;
+        }
+        var features = [];
+        for(var r in  records) {
+            var record = records[r];
+            if(record.bounds) {
+				var recordProjection = record.projection || "EPSG:4326";
+				var crs = oscar.Util.CoordinateReferences.getReference(recordProjection);
+				
+				var projection = new OpenLayers.Projection(crs.code);
+				var bbox = record.bounds.toArray(oscar.Util.isGeographicCRS(projection))
+				if(oscar.Util.isGeographicCRS(projection)) {
+				record.bounds = new OpenLayers.Bounds(
+					bbox[1],
+					bbox[0],
+					bbox[3],
+					bbox[2]
+				);
+				}
+				projection = new OpenLayers.Projection("EPSG:4326");
+
+                var features = oscar.Util.boundsToFeatures(record.bounds,projection,this.map);
+				var clones = []
+				for(var f in features) {
+					var feat = features[f].clone();
+					feat.style= {
+						strokeColor:"#004580",
+						fillOpacity:0.0,
+						strokeWidth:2
+					};
+					
+					clones.push(feat);
+				}
+				record.features=clones;
+				$$.merge(this.features,features);
+            }
+            this.addRecordToResultList(record);
+        }
+        this.renderFeaturesToMap();
+        $$(this.div).layout().resizeAll();
+        this.results.slimScroll({
+            height:"auto",
+            scrollTo:0
+        });
+
+    },
+    showSearchInfo:function(info) {
+        var nextDisabled=(info.nextRecord > info.numberOfRecordsMatched || info.nextRecord == 0)?true:false;
+        if(nextDisabled) 
+            this.$next.button("disable");
+        else 
+            this.$next.button("enable");
+
+        var previousDisabled=(info.nextRecord - info.numberOfRecordsReturned -1 <=0)?true:false;
+        if(previousDisabled) 
+            this.$previous.button("disable");
+        else 
+            this.$previous.button("enable");
+        
+        this.$buttons.css("visibility","visible");
+
+        var str = "";
+        var start = (info.nextRecord !=0)? info.nextRecord - info.numberOfRecordsReturned:1;
+        str+=start;
+        to = start + (info.numberOfRecordsReturned-1);
+        if(to > info.numberOfRecordsMatched) to = info.numberOfRecordsMatched;
+        str+= " - " + to;
+        str+= " of " + info.numberOfRecordsMatched;
+        this.$searchInfo.html(str);
+    },
+    enablePagination:function(info) {
+        
     },
     fadeInUI:function() {
     $$(".searching").show();
         $$(".searching").fadeOut();
         $$(this.div).fadeTo("fast",1);
+        
     },
 
     fadeOutUI:function() {
         $$(".searching").fadeIn();
         $$(this.div).fadeTo("fast",0.2);
+        
     },
     renderFeaturesToMap:function() {
-        var newGeoms = [];
-        var geomArray = []
-        for(var f in this.features) {
-            geomArray.push(this.features[f].geometry);
-        }
+		var layer = map.getLayersByName("results")[0];
+        if(this.features.length == 0) return;
+		
+		//extract the geometries from the features
+		var geometryArray=oscar.Util.extractGeometriesFromFeatures(this.features);
+		//attempt to combine geometries
+		geometryArray = oscar.Util.combineGeometries(geometryArray);
+		//flip the array and go in reverse 
+		geometryArray = oscar.Util.combineGeometries(geometryArray.reverse());
 
-        for(var f in this.features) {
-            var feature = this.features[f];
-            feature.style= {
-                fillColor:"#0a0a0a",
-                fillOpacity:0.3,
-                strokeWidth:2
-            };
-            
-            if(!oscar.Util.mergeToExistingGeometry(newGeoms,feature.geometry)) {
-                newGeoms.push(feature.geometry);
-            }
-        }
-        var masterFeature = new OpenLayers.Feature.Vector(
-            new OpenLayers.Geometry.MultiPolygon(newGeoms),
-            {name:"merged"}
-        );
+		/**
+		Create a new set of features from combined geometries to 
+		display on the map.
+		**/
+		for(var i = 0;i<geometryArray.length;i++) {
+			var feature = new OpenLayers.Feature.Vector(geometryArray[i]);
+			layer.addFeatures(feature);
+		}
+		map.zoomToExtent(layer.getDataExtent());
 
-        var layer = map.getLayersByName("results")[0];
-        layer.addFeatures(masterFeature);
-        map.zoomToExtent(masterFeature.geometry.getBounds());        
-    
     },
     addRecordToResultList:function(record) {
-        var $results = $$(this.div);
         var $result = $$("<div class='result'></div");
+        $result.css("position","relative");
         var $title = $$("<div class='title'></div>");
         var $abs = $$("<div class='abs'></div>");
         $result.append($title);
         $result.append($abs);
-
         var title = cswget.title(record) || cswget.identifier(record);
         var abs = cswget.abs(record);
         $title.html(title);
         $title.attr("title",title);
         $abs.html(abs);
         $abs.attr("title",abs);
-        $results.append($result);
+        this.results.append($result);
         $result.data("record",record);
-        
         this.attachMouseEvents($result,record);
         
 
     },
     attachMouseEvents:function($resultDiv,record) {
         $resultDiv.mouseenter(function(e) {
-        var layer = map.getLayersByName("results")[0];
-        layer.addFeatures(record.feature);
-
+        if(record.features) {
+            var layer = map.getLayersByName("results")[0];
+            layer.addFeatures(record.features);
+        }
         var $this = $$(this);
-        $this.addClass("over");
+        $this.switchClass("","over",100);
+        //$this.addClass("over");
         $this.children().each(function() {
             var $this = $$(this);
             $this.addClass("over");
@@ -168,11 +273,12 @@ oscar.Gui.CatalogueResults = new oscar.BaseClass(oscar.Gui,{
         });
         
         $resultDiv.mouseleave(function(e) {
-            var layer = map.getLayersByName("results")[0];
-            layer.removeFeatures(record.feature);
-
+            if(record.features) {
+                var layer = map.getLayersByName("results")[0];
+                layer.removeFeatures(record.features);
+            }
             var $this = $$(this);
-            $this.removeClass("over");
+            $this.switchClass("over","",100);
             $this.children().each(function() {
                 var $this = $$(this);
                 $this.removeClass("over");
@@ -180,32 +286,27 @@ oscar.Gui.CatalogueResults = new oscar.BaseClass(oscar.Gui,{
         });
         
         $resultDiv.click(function(e) {
-           map.zoomToExtent(record.feature.geometry.getBounds());
+           var $this = $$(this);
+           $this.parent().children().each(function() {
+            var $local = $$(this);
+            $local.removeClass("active");
+           });
+           
+           $this.addClass("active");
+           if(record.features) {
+				var bounds = record.features[0].geometry.getBounds();
+				for(var i = 1;i<record.features.length;i++) {
+					bounds.extend(record.features[i].geometry.getBounds());
+				}
+				map.zoomToExtent(bounds);
+           }
         });
     
     },
-    getFeatureFromRecord:function(record) {
-        var proj = record.projection || "EPSG:4326";
-        var crs = oscar.Util.CoordinateReferences.getReference(proj);
-        
-        var source = new OpenLayers.Projection(crs.code);
-        var destination = new OpenLayers.Projection("EPSG:900913")
-        var bounds = record.bounds.clone();
-        var polygon = bounds.toGeometry();
-        if(map.getProjectionObject().getCode() != source.getCode()) { //transform it
-            polygon.transform(source,destination);
-        }
-        
-        var feature = new OpenLayers.Feature.Vector(polygon,{"name":"temp"});
-        feature.style= {
-            strokeColor:"#004580",
-            fillOpacity:0.0,
-            strokeWidth:2
-        };
-        return feature;
-    },
     CLASS_NAME:"oscar.Gui.CatalogueResults"
 });
+
+
 
 var cswget = {
     title:function(r) {
