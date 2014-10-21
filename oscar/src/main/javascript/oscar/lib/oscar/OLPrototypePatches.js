49ada935ac7d92bc0c6c7b16e4af77c3d73ae191
troycoburn <troy.coburn@caris.com>
2014-10-21 09:04:45 -0300

    SFS-2914
    Adding a check for the bind function.

--- oscar/src/main/javascript/oscar/lib/oscar/OLPrototypePatches.js
+++ oscar/src/main/javascript/oscar/lib/oscar/OLPrototypePatches.js
@@ -851,3 +851,30 @@ OpenLayers.Format.CSWGetRecordById.v2_0_2.prototype.readers["csw"]["GetRecordByI
 	}
 	OpenLayers.Request.XMLHttpRequest = cXMLHttpRequest;
 })();
+
+/**
+* Checking for function availability for IE8
+*/
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                                 ? this
+                                 : oThis,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
