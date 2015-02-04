026b03e121baa3f73153cb2c9c382523f08127e7
troycoburn <troy.coburn@caris.com>
2015-02-04 07:19:56 -0400

    SFD-134
    Redesign of the panel to include event support. The layout plugin should function better.

--- oscar/src/main/javascript/oscar/lib/oscar/Util/Plugin/Download/Options.js
+++ oscar/src/main/javascript/oscar/lib/oscar/Util/Plugin/Download/Options.js
@@ -1,15 +1,29 @@
 oscar.Util.Plugin.Download.Options = new oscar.BaseClass(
 		oscar.Util.Plugin.Download,
 		{
+			EVENT_TYPES:['enterMode','exitMode'],
 			parent:null,
 			initialize : function(options) {
 				oscar.Util.Plugin.Download.prototype.initialize.apply(this,[ options ]);
-			},
-			clean:function() {
-				this.parent.toggleOptionsMode();
-				this.destroy();
+				this.events.on({
+					"enterMode":function() {
+						this.mode_container.show('slide',
+						{direction:"left"},
+						500);
+					},
+					"exitMode":function() {
+						var scope = this;
+						this.mode_container.hide('slide',
+						{direction:"left"},
+						500,$$.proxy(function(){
+							this.mode_container.empty();
+						},this));
+					},
+					scope:this
+				});
 			},
 			destroy:function() {
+				this.events.triggerEvent("exitMode");
 				
 			},
 			/**
@@ -17,20 +31,29 @@ oscar.Util.Plugin.Download.Options = new oscar.BaseClass(
 			* @see oscar.Util.Plugin
 			*/
 			play : function() {
+				this.events.triggerEvent("enterMode");
+				var scope = this;
 				this.container = $$("<div></div>");
+				this.container.addClass("options-mode-container");
 				this.container.css("width","100%");
 				this.container.css("height","100%");
+
+				this.mode_container.append(this.container);
+				
 				var scope = this;
 				
-				var north = $$("<div style='text-align:right'></div>");
-				this.$title_panel = $$("<div style='text-align:left'></div>");
-				var close_btn = $$("<button></button>").html("close");
-				close_btn.css("float","right");
-				this.$title_panel.append(close_btn);
-				this.$title_panel.css({
-					"white-space":"nowrap"
+				var north = $$("<div></div>");
+				var title_panel = $$("<div></div>");
+				this.$title=$$("<h2></h2>").html("&nbsp;");
+				var btn_panel = $$("<div></div>");
+				btn_panel.css({
+					"float":"right"
 				});
-				north.append(this.$title_panel);
+				var close_btn = $$("<button></button>").html("close");
+				btn_panel.append(close_btn);
+				title_panel.append(btn_panel);
+				title_panel.append(this.$title);
+				north.append(title_panel);
 				close_btn.button({
 					icons: {
 						primary: "ui-icon-closethick"
@@ -38,16 +61,18 @@ oscar.Util.Plugin.Download.Options = new oscar.BaseClass(
 					text: false,
 					label:"Close"
 				}).click(function(){
-					scope.clean();
-					
+					scope.destroy();
 				});
 				var center = $$("<div></div>");
 				north.addClass("ui-layout-north");
+				north.css("padding","5px");
 				center.addClass("ui-layout-center");
+				center.css("padding","5px");
 				this.container.append(north);
 				this.container.append(center);
 				
 				this.content_pane = $$("<div></div>");
+				this.content_pane.addClass("coverage-mode-content");
 				
 				center.append(this.content_pane);
 				this.content_pane.css({
@@ -55,34 +80,18 @@ oscar.Util.Plugin.Download.Options = new oscar.BaseClass(
 					"margin-right":"10px",
 					"height":"100% !important"
 				});
-				
-				
-				
-				setTimeout(function(){
-				window.onresize=function() {
-					scope.internal_layout.resizeAll();
-				}
-				scope.internal_layout = scope.container.layout({
-					resizable:false,
-					north: {
-						closable:false,
-					}
+				this.layout = this.container.layout();
+				$$(window).resize($$.proxy(function(){
+					this.layout.resizeAll();
+				},this));
+				var scope = this;
+
+				this.content_pane.slimScroll({
+					"height":"100%"
 				});
-				scope.content_pane.slimScroll({
-				"height":"100%"
-				});
-				},0);
-				this.parent.toggleOptionsMode(this.container);
 			},
 			setTitle:function(str) {
-				var tooltip = str;
-				if(str.length > 25) {
-					str = str.substring(0,25) + "&#8230;"
-				}
-				var $header = $$("<h2></h2>").html(str);
-				$header.attr("title",tooltip);
-				$header.tooltip();
-				this.$title_panel.append($header);
+				this.$title.html(str);
 			},
 			addOption:function(jDiv) {
 				this.content_pane.append(jDiv);
