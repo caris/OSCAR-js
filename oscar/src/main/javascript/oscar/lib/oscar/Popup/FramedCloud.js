bc012fe278d3e01c99633d4ee399274e29b38e1e
troycoburn <troy.coburn@caris.com>
2015-02-04 03:15:09 -0400

    OSCAR-0
    Fixing formatting.

--- oscar/src/main/javascript/oscar/lib/oscar/Popup/FramedCloud.js
+++ oscar/src/main/javascript/oscar/lib/oscar/Popup/FramedCloud.js
@@ -1,33 +1,33 @@
 /*
- * CARIS oscar - Open Spatial Component ARchitecture
- *
- * Copyright 2012 CARIS <http://www.caris.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * 	http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* CARIS oscar - Open Spatial Component ARchitecture
+*
+* Copyright 2012 CARIS <http://www.caris.com>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* 	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 /**
- * Class: oscar.FramedCloud
- * 
- * Inherits from: 
- * - <OpenLayers.Popup.FramedCloud>
- */
+* Class: oscar.FramedCloud
+* 
+* Inherits from: 
+* - <OpenLayers.Popup.FramedCloud>
+*/
 
 oscar.FramedCloud = oscar.BaseClass(OpenLayers.Popup.FramedCloud, {
 	/**
-	 * Property: positionBlocks
-	 * {Object} Hash of different position blocks, keyed by relativePosition
-	 *          two-character code string (ie "tl", "tr", "bl", "br")
-	 */
+	* Property: positionBlocks
+	* {Object} Hash of different position blocks, keyed by relativePosition
+	*          two-character code string (ie "tl", "tr", "bl", "br")
+	*/
 	positionBlocks : {
 		"tl" : {
 			'offset' :new OpenLayers.Pixel(51, 4),
@@ -131,21 +131,21 @@ oscar.FramedCloud = oscar.BaseClass(OpenLayers.Popup.FramedCloud, {
 		}
 	},
 	/**
-	 * Property: imageSize
-	 */
+	* Property: imageSize
+	*/
 	imageSize :new OpenLayers.Size(676, 736),
 	/**
-	 * Constructor: oscar.FramedCloud
-	 * 
-	 * Parameters: 
-	 * id - {String} a unique id of the popup. If null is passed an identifier will be automatically generated.
-	 * lonlat - {OpenLayers.LonLat} a longitude and latitude pair. The position on the map the popup will be shown.
-	 * contentSize - {OpenLayers.Size} a width/height pair size of the content.
-	 * contentHTML - {String} the HTML content to be displayed inside the popup.
-	 * anchor - {Object} object to which we�ll anchor the popup.
-	 * closeBox - {Boolean} whether to display a close box inside the popup. 
-	 * closeBoxCallback - {Function} Function to be called on closeBox click.
-	 */
+	* Constructor: oscar.FramedCloud
+	* 
+	* Parameters: 
+	* id - {String} a unique id of the popup. If null is passed an identifier will be automatically generated.
+	* lonlat - {OpenLayers.LonLat} a longitude and latitude pair. The position on the map the popup will be shown.
+	* contentSize - {OpenLayers.Size} a width/height pair size of the content.
+	* contentHTML - {String} the HTML content to be displayed inside the popup.
+	* anchor - {Object} object to which we�ll anchor the popup.
+	* closeBox - {Boolean} whether to display a close box inside the popup. 
+	* closeBoxCallback - {Function} Function to be called on closeBox click.
+	*/
 	initialize : function(id, lonlat, contentSize, contentHTML, anchor,
 			closeBox, closeBoxCallback) {
 		OpenLayers.Popup.FramedCloud.prototype.initialize
@@ -154,14 +154,14 @@ oscar.FramedCloud = oscar.BaseClass(OpenLayers.Popup.FramedCloud, {
 				+ "cloud_popup_relative_outlined_a.png";
 	},
 	/**
-	 * APIMethod: destroy
-	 */
+	* APIMethod: destroy
+	*/
 	destroy : function() {
 		OpenLayers.Popup.FramedCloud.prototype.destroy.apply(this, arguments);
 	},
 	/**
-	 * Constant: CLASS_NAME
-	 *  - oscar.Popup.FramedCloud 
-	 */
+	* Constant: CLASS_NAME
+	*  - oscar.Popup.FramedCloud 
+	*/
 	CLASS_NAME :'oscar.Popup.FramedCloud'
 });
